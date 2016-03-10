40af164b2abdf455f4e67e18c953df9a9d7601c4
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2016-03-10 15:33:20 +0100

    Add logging and fix Gitlab compatibility issues
    Signed-off-by: Mikko Koivunalho <mikko.koivunalho@iki.fi>

--- lib/Git/MoreHooks/TriggerJenkins.pm
+++ lib/Git/MoreHooks/TriggerJenkins.pm
@@ -229,11 +229,11 @@ use Carp;
 use Const::Fast;
 
 my $PKG = __PACKAGE__;
-(my $CFG = __PACKAGE__) =~ s/.*::/githooks./msx;
+( my $CFG = __PACKAGE__ ) =~ s/.*::/githooks./msx;
 
 const my $LAST_CHAR_IN_STRING => -1;
-const my $EMPTY_STRING => q{};
-const my $SPACE => q{ };
+const my $EMPTY_STRING        => q{};
+const my $SPACE               => q{ };
 
 =for Pod::Coverage setup_config configure_a_new_job trigger_branch
 
@@ -248,198 +248,214 @@ sub setup_config {
     my ($git) = @_;
 
     my $config = $git->get_config();
-    $config->{lc $CFG} //= {};
-    my $default = $config->{lc $CFG};
+    $log->debugf( 'setup_config(): Current Git config:\n%s.', $config );
+    $config->{ lc $CFG } //= {};
+    my $default = $config->{ lc $CFG };
 
     # Default values given as an array.
     # Array is default interpretation of config.
-    $default->{'create-new'}         //= ['1'];
-    $default->{'quiet'}              //= ['0'];
-    $default->{'force'}              //= ['0'];
-    $default->{'job-template'}       //= ['-unit-test JenkinsJobTemplate.tt2'];
-    $default->{'job-template-var'}   //= [];
+    $default->{'create-new'}       //= ['1'];
+    $default->{'quiet'}            //= ['0'];
+    $default->{'force'}            //= ['0'];
+    $default->{'job-template'}     //= ['-unit-test JenkinsJobTemplate.tt2'];
+    $default->{'job-template-var'} //= [];
+
     # TODO Fix create-new -> create-job or maybe opposite!
     # TODO Check valid values!
     return;
 }
 
 sub configure_a_new_job {
-    my ($git, $tpl_filename, %template_vars) = @_;
-    if (! eval { require Template; }) {
-        $git->error($PKG, 'Install Module Template (package Template-Toolkit)'
-            . ' to use this plugin!');
+    my ( $git, $tpl_filename, %template_vars ) = @_;
+    if ( !eval { require Template; } ) {
+        $git->error( $PKG, 'Install Module Template (package Template-Toolkit)' . ' to use this plugin!' );
         return;
     }
     my $config = {
         'INCLUDE_PATH' => Path::Tiny::path(q{.})->realpath,
-        'ENCODING' => 'utf8',
-        'INTERPOLATE' => 0,
-        'ANYCASE' => 0,
-        'ABSOLUTE' => 1,
-        'RELATIVE' => 1,
+        'ENCODING'     => 'utf8',
+        'INTERPOLATE'  => 0,
+        'ANYCASE'      => 0,
+        'ABSOLUTE'     => 1,
+        'RELATIVE'     => 1,
     };
     my $template = Template->new($config);
     my $xml_ready;
-    $template->process(
-        $tpl_filename,
-        \%template_vars,
-        \$xml_ready,
-    ) || $git->error($PKG, $template->error());
+    $template->process( $tpl_filename, \%template_vars, \$xml_ready, ) || $git->error( $PKG, $template->error() );
     return $xml_ready;
 }
 
 sub trigger_branch {
-    my ($git, $ref) = @_;
+    my ( $git, $ref ) = @_;
+    $log->tracef( 'Entering trigger_branch(%s)', $ref );
 
-    my $cache = $git->cache($PKG);
+    my $cache   = $git->cache($PKG);
     my $jenkins = $cache->{'jenkins'};
-    if (!defined $jenkins) {
+    if ( !defined $jenkins ) {
         croak('Internal error: No Jenkins in Git::Hooks cache!');
     }
-    my $user = $git->authenticated_user();
+    my $user = $git->authenticated_user() // '[undefined.user]';
 
     my %job_names;
-    if ($git->get_config($CFG => 'project')) {
-        $job_names{$git->get_config($CFG => 'project')} = 'dummy-filename';
+    if ( $git->get_config( $CFG => 'project' ) ) {
+        $job_names{ $git->get_config( $CFG => 'project' ) } = 'dummy-filename';
     } else {
-        %job_names = job_names_and_template_filenames($git, $ref);
+        %job_names = job_names_and_template_filenames( $git, $ref );
     }
 
     # If project not exists, create it.
-    foreach my $job_name (keys %job_names) {
-        my $this_job = get_job_from_jenkins($git, $job_name);
+    foreach my $job_name ( keys %job_names ) {
+        $log->debugf( 'Handle job (name:%s)', $job_name );
+
+        my $this_job = get_job_from_jenkins( $git, $job_name );
         my %job_info = (
             'description' => $job_name,
-            'branch' => $ref,
+            'branch'      => $ref,
         );
-        if (defined $git->get_config($CFG => 'email-domain')) {
-            $job_info{'recipients'} = $user . q{@}
-                . $git->get_config($CFG => 'email-domain');
+        if ( defined $git->get_config( $CFG => 'email-domain' ) ) {
+            $job_info{'recipients'} = $user . q{@} . $git->get_config( $CFG => 'email-domain' );
         }
-        if (defined $git->get_config($CFG => 'override-message-address')) {
+        if ( defined $git->get_config( $CFG => 'override-message-address' ) ) {
             my $overriding_addresses = $EMPTY_STRING;
-            my @overrides = $git->get_config($CFG => 'override-message-address');
+            my @overrides = $git->get_config( $CFG => 'override-message-address' );
             foreach my $override (@overrides) {
-                my ($refspec, $address) = split q{ }, $override, 2;
-                if ($ref =~ m/$refspec/msx) {
+                my ( $refspec, $address ) = split q{ }, $override, 2;
+                if ( $ref =~ m/$refspec/msx ) {
                     $overriding_addresses .= length $overriding_addresses > 0 ? $SPACE : $EMPTY_STRING;
                     $overriding_addresses .= $address;
                 }
             }
+
             # Override only if matching branch name was found!
-            if (exists $job_info{'recipients'} && $overriding_addresses) {
+            if ( exists $job_info{'recipients'} && $overriding_addresses ) {
                 $job_info{'recipients'} = $overriding_addresses;
             }
         }
-        foreach my $var ($git->get_config($CFG => 'job-template-var')) {
-            my ($var_name, $var_value) = split qr/=/msx, $var, 2;
+        foreach my $var ( $git->get_config( $CFG => 'job-template-var' ) ) {
+            my ( $var_name, $var_value ) = split qr/=/msx, $var, 2;
             $job_info{$var_name} = $var_value;
         }
-        my $xml_conf = configure_a_new_job($git, $job_names{$job_name}, %job_info);
-        $log->debug('New job: %s', $xml_conf);
-        $this_job = $jenkins->create_job($job_name, $xml_conf);
+        my $xml_conf = configure_a_new_job( $git, $job_names{$job_name}, %job_info );
+        $log->debugf( 'New job: %s', $xml_conf );
+        $this_job = $jenkins->create_job( $job_name, $xml_conf );
+
         # Trigger build
         my $triggered = $jenkins->trigger_build($job_name);
-        if (!defined $triggered) {
-            $git->error($PKG, "Failed to trigger job '$job_name'!");
+        if ( !defined $triggered ) {
+            $git->errorf( $PKG, "Failed to trigger job '%s'.", $job_name );
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
+                print "Failed to trigger job '$job_name'.\n";
+            }
             return;
         }
-        if ($git->get_config($CFG => 'quiet') eq '0') {
-            print "Job '$job_name' triggered to build.\n";
+        else {
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
+                print "Job '$job_name' triggered to build.\n";
+            }
         }
+        $log->debugf('All jobs triggered to Jenkins.');
 
         # Verify and tell user the status
         my $build_queue = $jenkins->build_queue();
         my $project_url;
         my $why;
-        foreach my $item (@{$build_queue->{'items'}}) {
-            if ($item->{'task'}->{'name'} eq $job_name) {
+
+        # TODO Write this to handle all jobs triggered by me!
+        foreach my $item ( @{ $build_queue->{'items'} } ) {
+            if ( $item->{'task'}->{'name'} eq $job_name ) {
                 $project_url = $item->{'task'}->{'url'};
-                $why = $item->{'why'};
+                $why         = $item->{'why'};
                 last;
             }
         }
-        if (defined $project_url) {
+        if ( defined $project_url ) {
             print "URL: $project_url\n";
         }
-        if (defined $why) {
+        if ( defined $why ) {
             print "Status: $why\n";
         }
     }
-   return 1;
+    $log->tracef( 'Exiting trigger_branch():%s', '1' );
+    return 1;
 }
 
 sub match_regexp_config_item {
-    my ($git, $item_name, $match) = @_;
-    my $item = $git->get_config($CFG => $item_name);
-    if ($item =~ m/^!/msx) {
-        $log->debug('Regexp starts with \'!\'. Match with negation!');
-        $item = substr $item, 1; # All string except first character.
+    my ( $git, $item_name, $match ) = @_;
+    my $item = $git->get_config( $CFG => $item_name );
+    if ( $item =~ m/^!/msx ) {
+        $log->debugf('Regexp starts with \'!\'. Match with negation!');
+        $item = substr $item, 1;    # All string except first character.
         return $match !~ m/$item/msx;
-    } else {
+    }
+    else {
         return $match =~ m/$item/msx;
     }
 }
 
 sub delete_job {
-    my ($git, $ref) = @_;
+    my ( $git, $ref ) = @_;
+    $log->tracef( 'Entering delete_job(%s)', $ref );
 
-    my $cache = $git->cache($PKG);
+    my $cache   = $git->cache($PKG);
     my $jenkins = $cache->{'jenkins'};
-    if (!defined $jenkins) {
+    if ( !defined $jenkins ) {
         croak('Internal error: No Jenkins in Git::Hooks cache!');
     }
-    my %job_names = job_names_and_template_filenames($git, $ref);
-    foreach my $job_name (keys %job_names) {
-        my $this_job = get_job_from_jenkins($git, $job_name);
-        if (defined $this_job) {
+    my %job_names = job_names_and_template_filenames( $git, $ref );
+    foreach my $job_name ( keys %job_names ) {
+        my $this_job = get_job_from_jenkins( $git, $job_name );
+        if ( defined $this_job ) {
+
             # Delete the job
             my $deleted = $jenkins->delete_project($job_name);
-            if (!defined $deleted) {
-                $git->error($PKG, "Failed to delete job '$job_name'!");
+            if ( !defined $deleted ) {
+                $git->error( $PKG, "Failed to delete job '$job_name'!" );
                 return;
             }
-            if ($git->get_config($CFG => 'quiet') eq '0') {
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
                 print "Job '$job_name' deleted from Jenkins.\n";
             }
-        } else {
-            if ($git->get_config($CFG => 'quiet') eq '0') {
+        }
+        else {
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
                 print "Job '$job_name' not in Jenkins. Not deleted.\n";
             }
         }
     }
+    $log->tracef( 'Exiting delete_job():%s', '1' );
     return 1;
 }
 
 sub job_names_and_template_filenames {
-    my ($git, $ref) = @_;
-    my ($branch) = $ref =~
-         m/^[^\/]+\/[^\/]+\/([[:graph:]]+)$/msx;
-    (my $job_name = $branch) =~ s/\//-/msx; # Replace slash-char with dash-char.
+    my ( $git, $ref ) = @_;
+    $log->tracef( 'Entering job_names_and_template_filenames(%s)', $ref );
+    my ($branch) = $ref =~ m/^[^\/]+\/[^\/]+\/([[:graph:]]+)$/msx;
+    ( my $job_name = $branch ) =~ s/\//-/msx;    # Replace slash-char with dash-char.
     ## no critic (ValuesAndExpressions::ProhibitCommaSeparatedStatements)
-    my %job_names = map {
-            $job_name . (split q{ })[0], (split q{ })[1]
-        } $git->get_config($CFG => 'job-template');
+    my %job_names = map { $job_name . ( split q{ } )[0], ( split q{ } )[1] } $git->get_config( $CFG => 'job-template' );
     ## use critic (ValuesAndExpressions::ProhibitCommaSeparatedStatements)
+    $log->tracef( 'Exiting job_names_and_template_filenames():%s', \%job_names );
     return %job_names;
 }
 
 # Get the job/project from Jenkins if it exists.
 sub get_job_from_jenkins {
-    my ($git, $job_name) = @_;
-    my $cache = $git->cache($PKG);
+    my ( $git, $job_name ) = @_;
+    my $cache   = $git->cache($PKG);
     my $jenkins = $cache->{'jenkins'};
-    if (!defined $jenkins) {
+    if ( !defined $jenkins ) {
         croak('Internal error: No Jenkins in Git::Hooks cache!');
     }
-    my $jobs = $jenkins->current_status({
+    my $jobs = $jenkins->current_status(
+        {
             'extra_params' => {
                 'tree' => 'jobs[name,color]'
             }
-        });
+        }
+    );
     my $this_job;
-    foreach my $job (@{$jobs->{'jobs'}}) {
-        if ($job->{'name'} eq $job_name) {
+    foreach my $job ( @{ $jobs->{'jobs'} } ) {
+        if ( $job->{'name'} eq $job_name ) {
             $this_job = $job;
             last;
         }
@@ -455,68 +471,75 @@ sub handle_affected_refs {
 
     # Connect to Jenkins if not already connected. Check from cache.
     my $cache = $git->cache($PKG);
-    if (! exists $cache->{'jenkins'}) {
-        if (! eval { require Jenkins::API; }) {
-            $git->error($PKG, 'Install Module Jenkins::API to use this plugin!');
+    if ( !exists $cache->{'jenkins'} ) {
+        if ( !eval { require Jenkins::API; } ) {
+            $git->error( $PKG, 'Install Module Jenkins::API to use this plugin!' );
             return;
         }
         my %jenkins_config;
         for my $option (qw/base-url api-key api-token/) {
-            $jenkins_config{$option} = $git->get_config($CFG => $option)
-                or $git->error($PKG, "missing $CFG.$option configuration attribute")
-                    and return;
+            $jenkins_config{$option} = $git->get_config( $CFG => $option )
+              or $git->error( $PKG, "missing $CFG.$option configuration attribute" )
+              and return;
         }
-        $jenkins_config{'base-url'} =~ s/[\/]{1,}$//msx; # trim trailing slashes from the URL
-        my $jenkins = Jenkins::API->new({
-          'base_url' => $jenkins_config{'base-url'},
-          'api_key'  => $jenkins_config{'api-key'},
-          'api_pass' => $jenkins_config{'api-token'},
-        });
+        $jenkins_config{'base-url'} =~ s/[\/]{1,}$//msx;    # trim trailing slashes from the URL
+        my $jenkins = Jenkins::API->new(
+            {
+                'base_url' => $jenkins_config{'base-url'},
+                'api_key'  => $jenkins_config{'api-key'},
+                'api_pass' => $jenkins_config{'api-token'},
+            }
+        );
         my $jenkins_version = $jenkins->check_jenkins_url();
-        if (! $jenkins_version) {
-            $git->error($PKG, 'Not able to connect Jenkins at address'
-                . " '$jenkins_config{'base-url'}'!");
+        if ( !$jenkins_version ) {
+            $git->error( $PKG, 'Not able to connect Jenkins at address' . " '$jenkins_config{'base-url'}'!" );
             return;
         }
         else {
-            if ($git->get_config($CFG => 'quiet') eq '0') {
-               print "Jenkins version: $jenkins_version\n";
-           }
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
+                print "Jenkins version: $jenkins_version\n";
+            }
         }
 
         # Set Jenkins in the cache.
         $cache->{'jenkins'} = $jenkins;
     }
 
-    foreach my $ref ($git->get_affected_refs()) {
-        if (! is_ref_enabled($ref, $git->get_config($CFG => 'ref'))) {
-            if ($git->get_config($CFG => 'quiet') eq '0') {
+    foreach my $ref ( $git->get_affected_refs() ) {
+        if ( !is_ref_enabled( $ref, $git->get_config( $CFG => 'ref' ) ) ) {
+            if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
                 print "Ref '$ref' not enabled.\n";
             }
             next;
         }
 
-        my $new_commit = ($git->get_affected_ref_range($ref))[1];
-        if ($new_commit =~ m/^[0]{1,}$/msx) { # Just zeros, deleted branch.
-            delete_job($git, $ref);
-        } else {
+        my $new_commit = ( $git->get_affected_ref_range($ref) )[1];
+        if ( $new_commit =~ m/^[0]{1,}$/msx ) {    # Just zeros, deleted branch.
+            delete_job( $git, $ref );
+        }
+        else {
             my @commit_ids = $git->get_affected_ref_commit_ids($ref);
+            $log->debugf( 'commit_ids (from get_affected_ref_commit_ids(%s):%s', $ref, \@commit_ids );
             my $nr_msg_ok_to_trigger = 0;
             foreach my $commit_id (@commit_ids) {
                 my $commit_msg = $git->get_commit_msg($commit_id);
-                $log->debug('commit message: \'%s\'.', (substr $commit_msg, 0, $LAST_CHAR_IN_STRING));
-                if (!$git->get_config($CFG => 'allow-commit-msg')) {
-                    $log->debug('No option allow-commit-msg. Allow all.');
+                $log->debugf( 'commit message: \'%s\'.', ( substr $commit_msg, 0, $LAST_CHAR_IN_STRING ) );
+                if ( !$git->get_config( $CFG => 'allow-commit-msg' ) ) {
+                    $log->debugf('No option allow-commit-msg. Allow all.');
                     $nr_msg_ok_to_trigger++;
+
+                    # TODO Shouldn't we have 'last' here?
                 }
-                if (match_regexp_config_item($git, 'allow-commit-msg', $commit_msg)) {
-                    $log->debug('Allowed commit msg.');
+                if ( match_regexp_config_item( $git, 'allow-commit-msg', $commit_msg ) ) {
+                    $log->debugf('Allowed commit msg.');
                     $nr_msg_ok_to_trigger++;
+
+                    # TODO Shouldn't we have 'next' here?
                 }
             }
-            $log->debug('nr_msg_ok_to_trigger: %s.', $nr_msg_ok_to_trigger);
-            if ($nr_msg_ok_to_trigger > 0) {
-                trigger_branch($git, $ref);
+            $log->debugf( 'nr_msg_ok_to_trigger: %s.', $nr_msg_ok_to_trigger );
+            if ( $nr_msg_ok_to_trigger > 0 ) {
+                trigger_branch( $git, $ref );
             }
         }
     }
@@ -531,17 +554,18 @@ sub handle_commit_at_client {
     my ($git) = @_;
 
     my $current_branch = $git->get_current_branch();
-    if (! is_ref_enabled($current_branch, $git->get_config($CFG => 'ref'))) {
-        if ($git->get_config($CFG => 'quiet') eq '0') {
+    if ( !is_ref_enabled( $current_branch, $git->get_config( $CFG => 'ref' ) ) ) {
+        if ( $git->get_config( $CFG => 'quiet' ) eq '0' ) {
             print "Ref '$current_branch' not enabled.\n";
         }
         return 1;
     }
+
     # TODO handle_ref()!
 }
 
 # Install hooks
-POST_COMMIT      \&handle_commit_at_client;
-POST_RECEIVE     \&handle_affected_refs;
+POST_COMMIT \&handle_commit_at_client;
+POST_RECEIVE \&handle_affected_refs;
 1;
 
